2ed4b13b1b9b6a5adc9c6dde0804e4bd4d5166f7
maxim <max.gavrushenko.tirare@gmail.com>
2018-06-13 16:21:45 +0300

    ZoomMount for current stores

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,52 +1,32 @@
-import React, { Component } from "react"
-import { compose , withProps, withState, withHandlers } from "recompose"
+import React, {Component} from "react"
+import {compose} from "recompose"
 import {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
     Marker,
-    InfoWindow,
-
+    InfoWindow
 } from "react-google-maps"
 
-const MapWithAMarker = compose(
-    withState('zoom', 'onZoomChange', 11),
-    withHandlers(() => {
-        const refs = {
-            map: undefined,
-        }
+const _ = require('lodash');
 
-        return {
-            onMapMounted: () => ref => {
-                refs.map = ref
-                console.log('onMapMounted')
-            },
-            onZoomChanged: ({ onZoomChange }) => () => {
-                onZoomChange(refs.map.getZoom(
-                    console.log('onZoomChanged')
-                ))
-            }
-        }
-    }),
-    withScriptjs,
-    withGoogleMap)
-    (props => {
+const MapWithAMarker = compose(withScriptjs, withGoogleMap)(props => {
+
+    const {onMapMounted, ...otherProps} = props;
+
     return (
-        <GoogleMap
-            zoom={props.zoom}
-            ref={props.onMapMounted}
-            onZoomChanged={props.onZoomChanged}
-            zoomToMarkers={props.zoomToMarkers}
-            defaultCenter={{ lat: props.stores[0].lat, lng: props.stores[0].lng }}>
+        <GoogleMap {...otherProps} ref={c => {
+            onMapMounted && onMapMounted(c)
+        }}>
             {props.stores.map(store => {
                 const onClick = props.onClick.bind(this, store)
-                {/*const zoomToMarkers = props.zoomToMarkers.bind(this.store)*/}
-
+                {/*console.log(props.stores);*/
+                }
                 return (
                     <Marker
                         key={store.id}
                         onClick={onClick}
-                        position={{ lat: store.lat, lng: store.lng }}
+                        position={{lat: store.lat, lng: store.lng}}
                         icon={{
                             path: 'M10.9,0C4.9,0,0,4.9,0,10.9c0,7,9.8,24.7,10.9,24.7c1.2,0,10.9-17.5,10.9-24.7C21.8,4.9,16.9,0,10.9,0z M10.9,15.5c-2.5,0-4.6-2-4.6-4.6s2-4.6,4.6-4.6c2.5,0,4.6,2,4.6,4.6S13.4,15.5,10.9,15.5z',
                             fillColor: store.storeTypeID === 1 ? '#882929' : '#000000',
@@ -54,7 +34,7 @@ const MapWithAMarker = compose(
                             scale: 1.1,
                             strokeWeight: 0,
                         }}
-                        >
+                    >
                         {props.selectedMarker === store &&
                         <InfoWindow>
                             <div>
@@ -75,77 +55,65 @@ export default class ShelterMap extends Component {
 
         this.state = {
             stores: props.stores,
-            selectedMarker: false,
-            markers: [],
-            onZoomChange: {}
+            selectedMarker: false
         }
-        this.zoomToMarkers = this.zoomToMarkers.bind(this);
     }
+
     componentWillReceiveProps(nextProps) {
-        console.log('componentWillReceiveProps method')
+
         // You don't have to do this check first, but it can help prevent an unneeded render
-        if (nextProps.stores !== this.state.stores) {
-            this.setState({ stores: nextProps.stores });
-            this.zoomToMarkers(nextProps.stores)
+        if (!(_.isEqual(nextProps.stores, this.state.stores))) {
+
+            console.log('componentWillReceiveProps');
+
+            this.zoomAndCenter(nextProps.stores)
         }
     }
 
-    zoomToMarkers(maps){
-        console.log(maps)
+    componentDidMounted(){
+        console.log('componentDidMounted');
+    }
+
+    zoomAndCenter(stores) {
         const bounds = new window.google.maps.LatLngBounds();
-        maps.forEach((map) => {
-            console.log(map)
-            console.log(Marker)
-            console.log(bounds)
-            if (map === Marker) {
-                let res = bounds.extend(new window.google.maps.LatLng(map.lat, map.lng));
-            }
+
+        stores.forEach((store) => {
+            bounds.extend(new window.google.maps.LatLng(store.lat, store.lng));
         })
-        // maps.fitBounds(res);
+        this._mapRef.fitBounds(bounds);
+
+        this.setState({stores: stores});
     }
 
+    handleMapMounted = (c) => {
+        if (!c || this._mapRef) return;
+        this._mapRef = c;
 
-    componentWillMount() {
-        console.log('componentWillMount method')
-        this.setState({ markers: [] })
-    }
+        this.zoomAndCenter(this.state.stores);
+    };
 
-    componentDidMount() {
-        console.log('componentDidMount method')
-        // this.setState({
-        //     zoomToMarkers: map => {
-        //         const bounds = new window.google.maps.LatLngBounds();
-        //         map.props.children.forEach((child) => {
-        //             if (child.type === Marker) {
-        //                 console.log(Marker)
-        //                 bounds.extend(new window.google.maps.LatLng(child.props.position.lat, child.props.position.lng));
-        //             }
-        //         })
-        //         map.fitBounds(bounds);
-        //     }
-        // })
-    }
-
-
-    handleClick (marker, event) {
-        console.log('handleClick method')
+    handleClick(marker, event) {
         // console.log({ marker })
-        this.setState({ selectedMarker: marker })
-
+        this.setState({selectedMarker: marker})
     }
+
     render() {
-        console.log('render method')
         return (
             <MapWithAMarker
                 selectedMarker={this.state.selectedMarker}
                 stores={this.state.stores}
                 onClick={this.handleClick.bind(this)}
-                zoomToMarkers={this.state.zoomToMarkers}
+                // zoomToMarkers={this.state.zoomToMarkers}
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkbu04rf_WBmWQhuo9c5K8DV1jrsK3Hlw&v=3.exp&libraries=geometry,drawing,places"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
+                loadingElement={<div style={{height: `100%`}}/>}
+                containerElement={<div style={{height: `400px`}}/>}
+                mapElement={<div style={{height: `100%`}}/>}
+                onMapMounted={this.handleMapMounted.bind(this)}
+                // onBoundsChanged={this.handleBoundsChanged}
+                defaultZoom={8}
+                defaultCenter={{lat: 29.5, lng: -95}}
             />
-        )
+
+        );
     }
 }
